c64a5c5681e0ece11e1f00965ca46c94edae0a9b
GLGuilherme <gabrielopes31@hotmail.com>
2019-09-27 17:19:56 -0300

    Backend

--- src/database/crud.js
+++ src/database/crud.js
@@ -1,21 +1,12 @@
 const connect = require("./connection");
 
-/*function cadastrarAluno() {
-  connect().query("select * from aluno", (err, res) => {
-    console.log(err, res);
-    connect().end();
-  });
-}*/
-
 const getUsers = (request, response) => {
   connect().query('create table ricardo(username varchar(50));', (error, results) => {
     if (error) {
       throw error
     }
-    //console.log(error, results);
     console.log(request.params.nome);
     
-    // response.status(200).json(results.rows)
     response.status(200).json({ user: request.params.nome })
   })
 }
--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const db = require('./database/crud')
-const http = require('http')
-const port = 3333
-const ip = 'localhost'
 
 app.use(bodyParser.json())
 app.use(
@@ -21,18 +18,3 @@ app.get('/teste/:nome', db.getUsers)
 
 
 app.listen(process.env.PORT || 3333);
-
-/*app.listen(port, ip, () => {
-  console.log(`App runing on port http://${ip}:${port}`)
-})*/
-
-/*const server = http.createServer((req, res) => {
-  console.log('Recebendo uma request!')
-  cadastrarAluno()
-  res.end('<h1>Aqui fica o que vamos enviar para o navegador como resposta!</h1>')
-})
-
-server.listen(port, ip, () => {
-  console.log(`Servidor rodando em http://${ip}:${port}`)
-  console.log('Para derrubar o servidor: ctrl + c');
-})*/
