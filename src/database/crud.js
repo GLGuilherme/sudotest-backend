2d8756a26f6b387ca7dd978c3c8f037410954f87
GLGuilherme <gabrielopes31@hotmail.com>
2019-09-27 16:47:02 -0300

    heroku

--- src/database/crud.js
+++ src/database/crud.js
@@ -13,7 +13,8 @@ const getUsers = (request, response) => {
       throw error
     }
     //console.log(error, results);
-    console.log(request.params.nome);
+    //console.log(request.params.nome);
+    console.log('oi')
     
     // response.status(200).json(results.rows)
     response.status(200).json( 'certo')
