8854861729a93505fb9e0339e101c7d303dcd80c
GLGuilherme <gabrielopes31@hotmail.com>
2019-09-27 16:28:06 -0300

    aa

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.use(
 )
 
 app.get('/', (request, response) => {
-  response.json({info: 'Node.js, Express, and Postgres API'})
+  response.json({'info: 'ricardo viadão''})
 })
 
 app.get('/teste/:nome', db.getUsers)
